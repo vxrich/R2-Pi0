969f677832a81eae323b9fe6b18873445d8428be
vxrich <riccardo.grespan6@gmail.com>
2016-12-02 13:55:15 +0100

    Test

--- app/src/main/java/com/example/juanito/r2_pi0/MainActivity.java
+++ app/src/main/java/com/example/juanito/r2_pi0/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+          setContentView(R.layout.activity_main);
 
         Toast.makeText(getApplicationContext(), "Sono vivo 1", Toast.LENGTH_SHORT).show();
 
